62ad2047c1e0067c8a1f3f15adf7f4ee3a1185e6
denisoliveiraa <denis_o@hotmail.com.br>
2024-07-30 23:24:58 -0300

    Adding db structure

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,47 @@
 import fastify  from "fastify";
-import { knex } from './database'
-
+import { knex } from "./database";
+import { z } from 'zod'
 const app = fastify()
 
-app.get('/hello', async () => {
-  const test = knex('sqlite_schema').select('*')
+app.post('/user', async (request, reply) => {
+  const createUser = z.object({
+    name: z.string(),
+    last_name: z.string()
+  })
 
-  return test
+  const {name, last_name} = createUser.parse(request.body)
+
+   await knex('user').insert({
+    id: crypto.randomUUID(),
+    name,
+    last_name,
+  })
+
+
+
+  return reply.status(201).send({'id': userID})
+
+})
+
+app.get('/user', async (request, reply) => {
+  
+  const listUser = await knex('user').select()
+
+  return {listUser}
+
+})
+
+app.get('/user/:id', async (request, reply) => {
+  const userIdSchema = z.object({
+    userID: z.string()
+  })
+
+  const {userID} = userIdSchema.parse(request.params)
+
+  const foundUser = knex('user').where('user.id', userID)
+
+  return {foundUser}
+
 })
 
 app.listen({port: 8523}).then(function() {console.log("Server is Running")})
